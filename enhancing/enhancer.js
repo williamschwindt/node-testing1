8b4e944f60db991ce0d43b076f13267a81b73d0b
William Schwindt <williammschwindt@gmail.com>
2020-05-12 16:18:54 -0700

    enhancment unit tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,24 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if(!item.name || !item.durability || !item.enhancment) {
+    throw new Error('item needs name, durability, and enhancment values')
+  }
+
+  if(item.enhancment > 20 || item.enhancment < 0) {
+    throw new Error('item enhancment must be between 0 and 20')
+  }
+  
+  if(item.enhancment === 20) {
+    return { ...item }
+  }
+
+  if(item.enhancment < 20) {
+    return {
+      ...item,
+      enhancment: item.enhancment + 1
+    };
+  }
 }
 
 function fail(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -16,3 +16,90 @@ describe('repair unit tests', () => {
         expect(() => enhancer.repair({})).toThrow()
     })
 })
+
+describe('enhancment success unit tests', () => {
+    test('enhancment is correctly changed', () => {
+        expect(enhancer.succeed({
+            name: 'hammer',
+            durability: 100,
+            enhancment: 15
+        })).toEqual({
+            name: 'hammer',
+            durability: 100,
+            enhancment: 16
+        })
+        expect(enhancer.succeed({
+            name: 'hammer',
+            durability: 100,
+            enhancment: 20
+        })).toEqual({
+            name: 'hammer',
+            durability: 100,
+            enhancment: 20
+        })
+        expect(() => enhancer.succeed({
+            name: 'hammer',
+            durability: 100,
+            enhancment: -1
+        })).toThrow()
+        expect(() => enhancer.succeed({
+            name: 'hammer',
+            durability: 100,
+            enhancment: 21
+        })).toThrow()
+    })
+
+    test('item name and durability are not changed', () => {
+        const item = enhancer.succeed({
+            name: 'hammer',
+            durability: 100,
+            enhancment: 20
+        })
+        expect(item.name).toBe('hammer')
+        expect(item.durability).toBe(100)
+    })
+
+    test('item has all keys', () => {
+        expect(() => enhancer.repair({})).toThrow()
+    })
+})
+
+describe('enhancment failure unit tests', () => {
+    test('item enhancment and durability changed correctly (for < en15)', () => {
+        expect(enhancer.failure({
+            name: 'hammer',
+            durability: 100,
+            enhancment: 14
+        })).toEqual({
+            name: 'hammer',
+            durability: 95,
+            enhancment: 14
+        })
+        expect(enhancer.failure({
+            name: 'hammer',
+            durability: 0,
+            enhancment: 14
+        })).toEqual({
+            name: 'hammer',
+            durability: 0,
+            enhancment: 14
+        })
+        expect(enhancer.failure({
+            name: 'hammer',
+            durability: 100,
+            enhancment: 14
+        })).toEqual({
+            name: 'hammer',
+            durability: 95,
+            enhancment: 14
+        })
+    })
+
+    test('', () => {
+        
+    })
+
+    test('', () => {
+        
+    })
+})
